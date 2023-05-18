564294f31e191407691219105793a92b7cc1da64
skilled5041 <skilled5041@gmail.com>
2023-05-18 11:34:16 -0400

    Fixed isDeathMessage regex to match periods.

--- src/utils/chat_message_regexes.ts
+++ src/utils/chat_message_regexes.ts
@@ -1,2 +1,2 @@
-export const isDeathMessage = /\x1b\[0m\x1b\[97m\x1b\[36m[a-zA-Z0-9_]{3,16} \x1b\[35m[a-zA-Z0-9_ \x1b\[]+\x1b\[0m/;
+export const isDeathMessage = /\x1b\[0m\x1b\[97m\x1b\[36m[a-zA-Z0-9_]{3,16} \x1b\[35m[a-zA-Z0-9_. \x1b\[]+\x1b\[0m/;
 export const isPlayerMessage = /^<([a-zA-Z0-9_]{3,16})>/;
